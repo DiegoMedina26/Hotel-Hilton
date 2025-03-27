9a0462082af953559d73fc0c59a7989ce842568a
DiegoMedina26 <diegitomedi973@gmail.com>
2025-03-27 14:49:50 -0500

    Areglo del login admin y pagina del administrador sin navbar ni footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { canActivateAdmin } from './guards/admin.guard'; // (Opcional) Proteger el login de admin
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { canActivateAdmin } from './guards/admin.guard'; // (Opcional) Protección
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent, canActivate: [canActivateAdmin] }, // Accesible solo por URL
+  { path: 'login', component: LoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [canActivateAdmin] }, // Protegido
   { path: '**', redirectTo: '' } // Redirección en caso de error
 ];
