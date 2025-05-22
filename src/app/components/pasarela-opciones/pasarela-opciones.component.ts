2e5921da6853e9cf8657be4becd95f036a507e85
Zyorel <dftorresr.1@gmail.com>
2025-05-21 23:25:23 -0500

    Fixed payment gateway

--- src/app/components/pasarela-opciones/pasarela-opciones.component.ts
+++ src/app/components/pasarela-opciones/pasarela-opciones.component.ts
@@ -5,30 +5,51 @@ import {
   Reservation,
   ReservationService,
 } from '../../services/reservations.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pasarela-opciones',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './pasarela-opciones.component.html',
   styleUrls: ['./pasarela-opciones.component.css'],
 })
 export class PasarelaOpcionesComponent {
   reserva!: Reservation;
 
+  nombreTitular: string = '';
+  numeroTarjeta: string = '';
+  vencimiento: string = '';
+  cvv: string = '';
+
   constructor(
     private router: Router,
     private reservationService: ReservationService
   ) {
     const nav = this.router.getCurrentNavigation();
+    const current = new Date();
+
     this.reserva = nav?.extras.state?.['reserva'];
+    this.reserva.reservationNumber = `RES-${current.getFullYear()}-${Math.floor(Math.random()*9999)}`
+  }
+
+  formularioValido(): boolean {
+    return (
+      this.nombreTitular !== '' &&
+      this.numeroTarjeta !== '' &&
+      this.vencimiento !== '' &&
+      this.cvv !== ''
+    );
   }
 
   pagarConStatus(statusId: string) {
     const reservaFinal = { ...this.reserva, statusId };
 
     this.reservationService.reserve(reservaFinal).subscribe({
-      next: () => alert('Reserva actualizada con status: ' + statusId),
+      next: () => {
+        alert('Reserva actualizada')
+        this.router.navigate(['/'])
+      },
       error: () => alert('Error al actualizar reserva.'),
     });
   }
